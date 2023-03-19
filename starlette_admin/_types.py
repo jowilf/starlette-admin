b9de08c18bb4c2d824ffa459a6d0b02eb46a669b
Jocelin Hounon <hounonj@gmail.com>
2023-03-19 18:17:36 -0500

    Update RequestAction docstrings

--- starlette_admin/_types.py
+++ starlette_admin/_types.py
@@ -5,11 +5,11 @@ class RequestAction(str, Enum):
     """Enumeration of string constants that represent different types of actions that can be performed on a ModelView.
 
     Attributes:
-        API (str): Used for select2 requests, which are requests for data to populate a select2 input field.
-        LIST (str): Used for listing objects in a view (datatable requests).
-        DETAIL (str): Used for displaying the details of a single object in a view.
-        CREATE (str): Used for displaying a form for creating a new object in a view.
-        EDIT (str): Used for displaying a form for editing an existing object in a view.
+        API: Used for select2 requests, which are requests for data to populate a select2 input field.
+        LIST: Used for listing objects in a view (datatable requests).
+        DETAIL: Used for displaying the details of a single object in a view.
+        CREATE: Used for displaying a form for creating a new object in a view.
+        EDIT: Used for displaying a form for editing an existing object in a view.
     """
 
     API = "API"
