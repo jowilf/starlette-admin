7126634592af8d8a7171dec570383e917b036d6b
giaptx <55086168+giaptx@users.noreply.github.com>
2023-03-21 09:47:31 +0700

    Allow input data in custom batch actions (#61)
    * feat: custom batch with params
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Allow input data in custom action
    
    * Implement changes
    
    * Update doc
    
    * Update custom actions example
    
    ---------
    
    Co-authored-by: root <root@localhost>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jocelin Hounon <hounonj@gmail.com>

--- starlette_admin/actions.py
+++ starlette_admin/actions.py
@@ -9,6 +9,7 @@ def action(
     confirmation: Optional[str] = None,
     submit_btn_text: Optional[str] = _("Yes, Proceed"),
     submit_btn_class: Optional[str] = "btn-primary",
+    form: Optional[str] = None,
 ) -> Callable[[Callable[..., Awaitable[str]]], Any]:
     """
     Use this decorator to add custom actions to your [ModelView][starlette_admin.views.BaseModelView]
@@ -21,6 +22,7 @@ def action(
         submit_btn_text: Submit button text
         submit_btn_class: Submit button variant (ex. `button-primary`, `button-ghost-info`,
                 `btn-outline-danger`, ...)
+        form: Custom form to collect data from user
 
 
     !!! usage
@@ -34,9 +36,20 @@ def action(
                 confirmation="Are you sure you want to mark selected articles as published ?",
                 submit_btn_text="Yes, proceed",
                 submit_btn_class="btn-success",
+                form='''
+                <form>
+                    <div class="mt-3">
+                        <input type="text" class="form-control" name="example-text-input" placeholder="Enter value">
+                    </div>
+                </form>
+                '''
             )
             async def make_published_action(self, request: Request, pks: List[Any]) -> str:
                 # Write your logic here
+
+                data: FormData =  await request.form()
+                user_input = data.get("example-text-input")
+
                 if ... :
                     # Display meaningfully error
                     raise ActionFailed("Sorry, We can't proceed this action now.")
@@ -52,6 +65,7 @@ def action(
             "confirmation": confirmation,
             "submit_btn_text": submit_btn_text,
             "submit_btn_class": submit_btn_class,
+            "form": form if form is not None else "",
         }
         return f
 
