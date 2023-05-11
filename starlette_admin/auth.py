6fb7d366e9200809c3e08199ab3f31160296d993
Mike Artemiev <mixartemev@gmail.com>
2023-05-12 01:31:02 +0300

    Allow Unauthenticated Access to js.cookie.min.js (#183)
    * fixes issue 182
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Remove change from starlette_admin/base.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Jocelin Hounon <hounonj@gmail.com>

--- starlette_admin/auth.py
+++ starlette_admin/auth.py
@@ -156,6 +156,7 @@ class AuthMiddleware(BaseHTTPMiddleware):
                 "/statics/css/fontawesome.min.css",
                 "/statics/js/vendor/jquery.min.js",
                 "/statics/js/vendor/tabler.min.js",
+                "/statics/js/vendor/js.cookie.min.js",
             ]
         )  # Allow static files needed for the login page
         self.allow_paths.extend(
