fa4bfbd5fc09dcd710148980d34144e0dcb13a21
Jocelin Hounon <hounonj@gmail.com>
2023-07-01 15:11:25 -0500

    Update example

--- examples/authlib/config.py
+++ examples/authlib/config.py
@@ -4,6 +4,6 @@ DATABASE_FILE = "sample_db.sqlite"
 ENGINE_URI = "sqlite:///" + DATABASE_FILE
 
 SECRET = "1234567890"
-AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID", "xxxxxxx")
-AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET", "xxxxxx")
-AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN", "xxxxxx.auth0.com")
+AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID", "your-auth0-client-id")
+AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET", "your-auth0-client-secret")
+AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN", "your-auth0-domain")
