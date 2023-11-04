8ea55ad2a5b746e4f7e5f80c794bb6b5cda0f4bd
hasansezertasan <hasansezertasan@gmail.com>
2023-11-04 11:34:40 +0300

    More fields added.

--- examples/fields/app.py
+++ examples/fields/app.py
@@ -1,5 +1,11 @@
+import datetime
+import enum
+
 from sqlalchemy import (
     Column,
+    DateTime,
+    JSON,
+    Enum,
     Integer,
     Text,
     create_engine,
@@ -9,17 +15,34 @@ from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
 from starlette.routing import Route
 from starlette_admin.contrib.sqla import Admin, ModelView
-from starlette_admin.fields import IntegerField, SimpleMDEField, TinyMCEEditorField
+from starlette_admin.fields import DateTimeField, IntegerField, SimpleMDEField, TinyMCEEditorField, URLField, EmailField, PhoneField, ColorField, PasswordField, EnumField, JSONField, TextAreaField, StringField
 
 Base = declarative_base()
 
+
+class ExampleEnum(str, enum.Enum):
+    OPTION_1 = "Option 1"
+    OPTION_2 = "Option 2"
+    OPTION_3 = "Option 3"
 
 class Field(Base):
     __tablename__ = "field"
 
     id = Column(Integer, primary_key=True)
+    date_created = Column(DateTime, default=datetime.datetime.utcnow)
+    date_updated = Column(DateTime, default=datetime.datetime.utcnow)
     tinymce = Column(Text, nullable=False)
     simplemde = Column(Text, nullable=False)
+    text = Column(Text, nullable=False)
+    textarea = Column(Text, nullable=False)
+    url = Column(Text, nullable=False)
+    email = Column(Text, nullable=False)
+    phone = Column(Text, nullable=False)
+    color = Column(Text, nullable=False)
+    password = Column(Text, nullable=False)
+    enum_1 = Column(Enum(ExampleEnum), nullable=False)
+    enum_2 = Column(Text, nullable=False)
+    json = Column(JSON, nullable=False)
 
 
 class FieldView(ModelView):
@@ -29,6 +52,20 @@ class FieldView(ModelView):
             label="ID",
             read_only=True,
         ),
+        DateTimeField(
+            name="date_created",
+            label="Date Created",
+            read_only=True,
+            exclude_from_create=True,
+            exclude_from_edit=True,
+        ),
+        DateTimeField(
+            name="date_updated",
+            label="Date Updated",
+            read_only=True,
+            exclude_from_create=True,
+            exclude_from_edit=True,
+        ),
         TinyMCEEditorField(
             name="tinymce",
             label="TinyMCEEditorField",
@@ -36,6 +73,52 @@ class FieldView(ModelView):
         SimpleMDEField(
             name="simplemde",
             label="SimpleMDEField",
+        ),
+        StringField(
+            name="text",
+            label="StringField",
+        ),
+        TextAreaField(
+            name="textarea",
+            label="TextAreaField",
+        ),
+        URLField(
+            name="url",
+            label="URLField",
+        ),
+        EmailField(
+            name="email",
+            label="EmailField",
+        ),
+        PhoneField(
+            name="phone",
+            label="PhoneField",
+        ),
+        ColorField(
+            name="color",
+            label="ColorField",
+        ),
+        PasswordField(
+            name="password",
+            label="PasswordField",
+        ),
+        EnumField(
+            name="enum_1",
+            label="EnumField",
+            enum=ExampleEnum,
+        ),
+        EnumField(
+            name="enum_2",
+            label="EnumField",
+            choices=[
+                ("OPTION_4", "Option 4"),
+                ("OPTION_5", "Option 5"),
+                ("OPTION_6", "Option 6"),
+            ],
+        ),
+        JSONField(
+            name="json",
+            label="JSONField",
         ),
     ]
 
