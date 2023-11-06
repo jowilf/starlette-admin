jowilf/starlette-admin

commit ccdf8b5ef758fdb8c94069737184eff69353373f

Detect SQLAlchemy primary key with custom column name  (#361)
* Include custom SQLAlchemy column name with space and special character in the Test suite

* Fix #347

* Fix linting

* Fix CI

* Add changelog

* Format

starlette_admin/contrib/sqla/view.py
@@ -4,7 +4,12 @@ import anyio.to_thread
 from sqlalchemy import Column, String, cast, func, inspect, or_, select
 from sqlalchemy.exc import NoInspectionAvailable, SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import InstrumentedAttribute, Mapper, Session, joinedload
+from sqlalchemy.orm import (
+    InstrumentedAttribute,
+    Mapper,
+    Session,
+    joinedload,
+)
 from sqlalchemy.sql import Select
 from starlette.requests import Request
 from starlette.responses import Response
@@ -65,7 +70,7 @@ class ModelView(BaseModelView):
         self.name = name or self.name or prettify_class_name(self.model.__name__)
         self.icon = icon
         self._pk_column: Column = mapper.primary_key[0]
-        self.pk_attr = self._pk_column.key
+        self._setup_primary_key()
         self._pk_coerce = extract_column_python_type(self._pk_column)
         if self.fields is None or len(self.fields) == 0:
             self.fields = [
@@ -100,6 +105,14 @@ class ModelView(BaseModelView):
             self.fields_default_sort, is_default_sort_list=True
         )
         super().__init__()
+
+    def _setup_primary_key(self) -> None:
+        # Detect the primary key attribute of the model
+        for key in self.model.__dict__:
+            attr = getattr(self.model, key)
+            if isinstance(attr, InstrumentedAttribute) and attr.primary_key:
+                self.pk_attr = key
+                return
 
     async def handle_action(
         self, request: Request, pks: List[Any], name: str

tests/sqla/test_sync_engine.py
@@ -41,8 +41,8 @@ class Brand(str, enum.Enum):
 
 class Product(Base):
     __tablename__ = "product"
-    id = Column(Integer, primary_key=True)
-    title = Column(String(100))
+    id = Column("Product / ID", Integer, primary_key=True)
+    title = Column("Product / Title", String(100))
     description = Column(Text)
     price = Column(Float)
     brand = Column(Enum(Brand))

commit 9646fbed4e262db5fe6e47b214ef464cfbd8d0fa

Fixed a regression caused by #361 where SQLAlchemy models with Mixin Classes raises AttributeError (#385)
* Solve 383

* [pre-commit.ci] auto fixes from pre-commit.com hooks

for more information, see https://pre-commit.ci

* Altered test_sqla_and_pydantic.py to use mixins for ID columns.

* Ignore mypy Errors.

* Implement the easiest fix, avoiding the need to inspect the model twice.

* Add changelog

* Add changelog

---------

Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
Co-authored-by: Jocelin Hounon <hounonj@gmail.com>

starlette_admin/contrib/sqla/view.py
@@ -93,7 +93,7 @@ class ModelView(BaseModelView):
         )
         self.name = name or self.name or prettify_class_name(self.model.__name__)
         self.icon = icon
-        self._pk_column: Column = mapper.primary_key[0]
+        self._pk_column: Column = next(iter(mapper.local_table.primary_key))  # type: ignore
         self._setup_primary_key()
         self._pk_coerce = extract_column_python_type(self._pk_column)
         if self.fields is None or len(self.fields) == 0:
@@ -134,7 +134,9 @@ class ModelView(BaseModelView):
         # Detect the primary key attribute of the model
         for key in self.model.__dict__:
             attr = getattr(self.model, key)
-            if isinstance(attr, InstrumentedAttribute) and attr.primary_key:
+            if isinstance(attr, InstrumentedAttribute) and getattr(
+                attr, "primary_key", False
+            ):
                 self.pk_attr = key
                 return
 

tests/sqla/test_sqla_and_pydantic.py
@@ -29,19 +29,21 @@ pytestmark = pytest.mark.asyncio
 Base = declarative_base()
 
 
-class User(Base):
+class IDMixin:
+    id = Column(Integer, primary_key=True)
+
+
+class User(Base, IDMixin):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
     name = Column(String(100))
 
     todos = relationship("Todo", back_populates="user")
 
 
-class Todo(Base):
+class Todo(Base, IDMixin):
     __tablename__ = "todo"
 
-    id = Column(Integer, primary_key=True)
     todo = Column(String(255))
     completed = Column(Boolean)
     deadline = Column(DateTime)