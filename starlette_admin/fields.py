3a3df85a0cc56bd73704886c9689b3cafb14ade3
Hasan Sezer Taşan <13135006+hasansezertasan@users.noreply.github.com>
2023-12-29 05:41:25 +0300

    Removed dobule spaces, fixed some broken admonition syntax, added single spaces before and after headings. (#446)
    * Removed dobule empty lines, fixed some broken admonition syntax.
    
    * Remove unnecessary argument for func.count
    
    ---------
    
    Co-authored-by: Jocelin Hounon <hounonj@gmail.com>

--- starlette_admin/fields.py
+++ starlette_admin/fields.py
@@ -108,6 +108,7 @@ class BaseField:
         However, this function can be overridden to provide custom logic for computing the value of a field.
 
         ??? Example
+
             ```py
             # Suppose we have a `User` model with `id`, `first_name`, and `last_name` fields.
             # We define a custom field called `MyCustomField` to compute the full name of the user:
@@ -145,6 +146,7 @@ class BaseField:
         """Formats a value for sending to the frontend based on the current request action.
 
         !!! important
+
             Make sure this value is JSON Serializable for RequestAction.LIST and RequestAction.API
 
         Args:
@@ -469,7 +471,7 @@ class EnumField(StringField):
     It takes a python `enum.Enum` class or a list of *(value, label)* pairs.
     It can also be a list of only values, in which case the value is used as the label.
     Example:
-        ```Python
+        ```python
         class Status(str, enum.Enum):
             NEW = "new"
             ONGOING = "ongoing"
@@ -482,7 +484,7 @@ class EnumField(StringField):
             fields = [EnumField("status", enum=Status)]
         ```
 
-        ```Python
+        ```python
         class MyModel:
             language: str
 
@@ -958,6 +960,7 @@ class RelationField(BaseField):
     between your models.
 
     !!! important
+
         It is important to add both models in your admin interface.
 
     Parameters:
@@ -965,6 +968,7 @@ class RelationField(BaseField):
 
 
     ??? Example
+
         ```py
         class Author:
             id: Optional[int]
@@ -1054,7 +1058,8 @@ class HasMany(RelationField):
 class CollectionField(BaseField):
     """
     This field represents a collection of others fields. Can be used to represent embedded mongodb document.
-    !!!usage
+    !!! usage
+
     ```python
      CollectionField("config", fields=[StringField("key"), IntegerField("value", help_text="multiple of 5")]),
     ```
@@ -1134,7 +1139,8 @@ class ListField(BaseField):
     Encapsulate an ordered list of multiple instances of the same field type,
     keeping data as a list.
 
-    !!!usage
+    !!! usage
+
         ```python
         class MyModel:
             id: Optional[int]
