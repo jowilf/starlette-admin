7a6b6a135973fe5572db7fa2a72cb1fa216c665c
Jocelin Hounon <hounonj@gmail.com>
2024-01-10 14:48:01 -0600

    Minor improvements

--- starlette_admin/fields.py
+++ starlette_admin/fields.py
@@ -273,7 +273,7 @@ class TinyMCEEditorField(TextAreaField):
         statusbar: Show/hide the statusbar in the editor
         toolbar: Toolbar options to show in the editor
         content_style: CSS style to apply to the editor content
-        data_options: Other options to pass to TinyMCE
+        extra_options: Other options to pass to TinyMCE
     """
 
     class_: str = "field-tinymce-editor form-control"
@@ -285,7 +285,7 @@ class TinyMCEEditorField(TextAreaField):
     statusbar: bool = False
     toolbar: str = "undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat"
     content_style: str = "body { font-family: -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif; font-size: 14px; -webkit-font-smoothing: antialiased; }"
-    data_options: Dict[str, Any] = dc_field(default_factory=dict)
+    extra_options: Dict[str, Any] = dc_field(default_factory=dict)
     """For more options, see the [TinyMCE | Configuration](https://www.tiny.cloud/docs-3x/reference/Configuration3x/)"""
 
     def additional_js_links(self, request: Request, action: RequestAction) -> List[str]:
@@ -297,17 +297,20 @@ class TinyMCEEditorField(TextAreaField):
         return []
 
     def input_params(self) -> str:
-        if self.data_options.get("height", None) is None:
-            self.data_options["height"] = self.height
-        if self.data_options.get("menubar", None) is None:
-            self.data_options["menubar"] = self.menubar
-        if self.data_options.get("statusbar", None) is None:
-            self.data_options["statusbar"] = self.statusbar
-        if self.data_options.get("toolbar", None) is None:
-            self.data_options["toolbar"] = self.toolbar
-        if self.data_options.get("content_style", None) is None:
-            self.data_options["content_style"] = self.content_style
-        return html_params({"data-options": json.dumps(self.data_options)})
+        _options = {
+            "height": self.height,
+            "menubar": self.menubar,
+            "statusbar": self.statusbar,
+            "toolbar": self.toolbar,
+            "content_style": self.content_style,
+            **self.extra_options,
+        }
+
+        return (
+            super().input_params()
+            + " "
+            + html_params({"data-options": json.dumps(_options)})
+        )
 
 
 @dataclass
