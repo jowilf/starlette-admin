9e50bc9f02828d1f22c04c63a9c629038c765f86
Peter Stein <peterstein@protonmail.com>
2024-09-01 00:11:32 +0200

    Fix Deprecation Warnings for TemplateResponse and Jinja2Templates (#575)
    * Ignore .venv
    
    * Fix DeprecationWarning: The `name` is not the first parameter anymore.
    
    Addresses https://github.com/jowilf/starlette-admin/issues/574
    
    DeprecationWarning: The `name` is not the first parameter anymore. The first parameter should be the `Request` instance.
      Replace `TemplateResponse(name, {"request": request})` by `TemplateResponse(request, name)`.
        warnings.warn(
    
    Before Starlette 0.29.0, the name was the first parameter.
    
    Also, before that, in previous versions, the request object was passed as part of the key-value pairs in the context for Jinja2.
    
    See:
    - https://www.starlette.io/release-notes/#0290
    - https://github.com/encode/starlette/pull/2191
    - https://github.com/encode/starlette/blob/c78c9aac17a4d68e0647252310044502f1b7da71/starlette/templating.py#L166-L178
    - https://fastapi.tiangolo.com/reference/templating/#fastapi.templating.Jinja2Templates.TemplateResponse
    - https://fastapi.tiangolo.com/advanced/templates/#using-jinja2templates
    
    * Fix DeprecationWarning: Extra environment options are deprecated.
    
    Resolves https://github.com/jowilf/starlette-admin/issues/574
    
    According to the Starlette 0.28.0 release notes, the **env_options parameter in Jinja2Templates has been deprecated in favor of the new env parameter. The relevant pull request #2159 explains this change.
    
    See:
    - https://github.com/encode/starlette/pull/2159
    - https://www.starlette.io/release-notes/#0280
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- starlette_admin/base.py
+++ starlette_admin/base.py
@@ -2,7 +2,7 @@ import json
 from json import JSONDecodeError
 from typing import Any, Awaitable, Callable, Dict, List, Optional, Sequence, Type, Union
 
-from jinja2 import ChoiceLoader, FileSystemLoader, PackageLoader
+from jinja2 import ChoiceLoader, Environment, FileSystemLoader, PackageLoader
 from starlette.applications import Starlette
 from starlette.datastructures import FormData
 from starlette.exceptions import HTTPException
@@ -190,13 +190,17 @@ class BaseAdmin:
             self._views.append(self.index_view)
 
     def _setup_templates(self) -> None:
-        templates = Jinja2Templates(self.templates_dir, extensions=["jinja2.ext.i18n"])
-        templates.env.loader = ChoiceLoader(
-            [
-                FileSystemLoader(self.templates_dir),
-                PackageLoader("starlette_admin", "templates"),
-            ]
+        env = Environment(
+            loader=ChoiceLoader(
+                [
+                    FileSystemLoader(self.templates_dir),
+                    PackageLoader("starlette_admin", "templates"),
+                ]
+            ),
+            extensions=["jinja2.ext.i18n"],
         )
+        templates = Jinja2Templates(env=env)
+
         # globals
         templates.env.globals["views"] = self._views
         templates.env.globals["app_title"] = self.title
@@ -374,9 +378,9 @@ class BaseAdmin:
         if not model.is_accessible(request):
             raise HTTPException(HTTP_403_FORBIDDEN)
         return self.templates.TemplateResponse(
-            model.list_template,
-            {
-                "request": request,
+            request=request,
+            name=model.list_template,
+            context={
                 "model": model,
                 "title": model.title(request),
                 "_actions": await model.get_all_actions(request),
@@ -395,9 +399,9 @@ class BaseAdmin:
         if obj is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         return self.templates.TemplateResponse(
-            model.detail_template,
-            {
-                "request": request,
+            request=request,
+            name=model.detail_template,
+            context={
                 "title": model.title(request),
                 "model": model,
                 "raw_obj": obj,
@@ -410,11 +414,15 @@ class BaseAdmin:
         request.state.action = RequestAction.CREATE
         identity = request.path_params.get("identity")
         model = self._find_model_from_identity(identity)
-        config = {"request": request, "title": model.title(request), "model": model}
+        config = {"title": model.title(request), "model": model}
         if not model.is_accessible(request) or not model.can_create(request):
             raise HTTPException(HTTP_403_FORBIDDEN)
         if request.method == "GET":
-            return self.templates.TemplateResponse(model.create_template, config)
+            return self.templates.TemplateResponse(
+                request=request,
+                name=model.create_template,
+                context=config,
+            )
         form = await request.form()
         dict_obj = await self.form_to_dict(request, form, model, RequestAction.CREATE)
         try:
@@ -427,8 +435,9 @@ class BaseAdmin:
                 }
             )
             return self.templates.TemplateResponse(
-                model.create_template,
-                config,
+                request=request,
+                name=model.create_template,
+                context=config,
                 status_code=HTTP_422_UNPROCESSABLE_ENTITY,
             )
         pk = await model.get_pk_value(request, obj)
@@ -452,14 +461,17 @@ class BaseAdmin:
         if obj is None:
             raise HTTPException(HTTP_404_NOT_FOUND)
         config = {
-            "request": request,
             "title": model.title(request),
             "model": model,
             "raw_obj": obj,
             "obj": await model.serialize(obj, request, RequestAction.EDIT),
         }
         if request.method == "GET":
-            return self.templates.TemplateResponse(model.edit_template, config)
+            return self.templates.TemplateResponse(
+                request=request,
+                name=model.edit_template,
+                context=config,
+            )
         form = await request.form()
         dict_obj = await self.form_to_dict(request, form, model, RequestAction.EDIT)
         try:
@@ -472,8 +484,9 @@ class BaseAdmin:
                 }
             )
             return self.templates.TemplateResponse(
-                model.edit_template,
-                config,
+                request=request,
+                name=model.edit_template,
+                context=config,
                 status_code=HTTP_422_UNPROCESSABLE_ENTITY,
             )
         pk = await model.get_pk_value(request, obj)
@@ -493,8 +506,9 @@ class BaseAdmin:
     ) -> Response:
         assert isinstance(exc, HTTPException)
         return self.templates.TemplateResponse(
-            "error.html",
-            {"request": request, "exc": exc},
+            request=request,
+            name="error.html",
+            context={"exc": exc},
             status_code=exc.status_code,
         )
 
