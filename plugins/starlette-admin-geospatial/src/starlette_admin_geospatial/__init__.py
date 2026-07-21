5c72e25cb53bc2145a92e442d82b6ea60845ae8d
Jocelin Hounon <hounonj@gmail.com>
2026-07-20 22:38:42 -0700

    Remove plugin example

--- plugins/starlette-admin-geospatial/src/starlette_admin_geospatial/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.1.0"
-
-from .fields import GeospatialRatingField as GeospatialRatingField
-from .plugin import GeospatialConfig as GeospatialConfig
-from .plugin import GeospatialPlugin as GeospatialPlugin
